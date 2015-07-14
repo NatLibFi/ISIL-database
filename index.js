a9a1ac1020e380a97b728cc2319b3076e85141e4
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2015-07-14 10:47:31 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+var handlebars = require('express-handlebars')
+.create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+// Root
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.get('/admin', function(req, res) {
+  res.render('admin');
+});
+
+// 404
+
+app.use(function(req, res, next){
+  res.status(404);
+  res.render('404');
+});
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});
