c31be74e34d9d21ab839725dcd72ac367fe4b0b8
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2015-07-14 13:29:59 +0300

    Second commit

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+/* jshint node:true */
 'use strict';
+
+var fortune = require('./lib/fortune.js');
 
 var express = require('express');
 
+var bodyParser = require('body-parser');
+
 var app = express();
 
-var handlebars = require('express-handlebars')
-.create({ defaultLayout: 'main' });
+var handlebars = require('express-handlebars').create({
+  defaultLayout:'main',
+  helpers: {
+    section: function(name, options) {
+      if(!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    }
+  }
+});
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -13,30 +27,55 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+// Middleware for handling the queries submitted using the POST method
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.get('/newsletter', function(req, res){
+// we will learn about CSRF later...for now, we just
+// provide a dummy value
+  res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+
+app.post('/process', function(req, res){
+  console.log('Form (from querystring): ' + req.query.form);
+  console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+  console.log('Name (from visible form field): ' + req.body.name);
+  console.log('Email (from visible form field): ' + req.body.email);
+  res.redirect(303, '/thank-you');
+});
+
 // Root
 
 app.get('/', function(req, res) {
   res.render('home');
 });
 
+// Admin page
+
 app.get('/admin', function(req, res) {
-  res.render('admin');
+  res.render('admin', { fortune: fortune.getFortune() });
 });
 
 // 404
 
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
   res.status(404);
   res.render('404');
 });
+
 // 500 error handler (middleware)
-app.use(function(err, req, res, next){
+app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.status(500);
   res.render('500');
 });
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function() {
   console.log( 'Express started on http://localhost:' +
     app.get('port') + '; press Ctrl-C to terminate.' );
 });
