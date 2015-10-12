61deb738ffbeed1b3ed973b937f17b3ef5872d8a
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2015-10-12 15:59:51 +0300

    Tietokannan generointi

--- CSVToMongo.js
+++ CSVToMongo.js
@@ -2,6 +2,7 @@
 'use strict';
 
 var fs = require('fs');
+var _ = require('underscore');
 var MongoClient = require('mongodb').MongoClient;
 var mongoUrl = 'mongodb://localhost:27017/isil';
 
@@ -9,10 +10,11 @@ var file = process.argv[2];
 
 var totalData = [];
 
-var Library = function Library(name, isil, linda, former) {
+var Library = function Library(name, isil, linda, cities, former) {
   this.name = name;
   this.isil = isil;
   this.linda = linda;
+  this.cities = cities; 
   this.former = former;
 };
 
@@ -28,15 +30,26 @@ fs.readFile(file, 'utf-8', function(err, data) {
   else {
     var list = data.split('\n');
     list.forEach(function(item) {
-      var items = item.split(',');
+      var items = item.split(';');
       if (items.length > 1) {
-        var library = new Library(items[3], items[2], items[0], items[1]);
-        totalData.push(library);
+        var cities = [];
+        if (items.length >= 5) {
+          cities = items[4].split(',');
+          cities = _.map(cities, function (city) { return city.trim(); });
+        }
+        var library = new Library(items[3], items[2], items[0], cities, items[5]);
+        if (library.name !== '') {
+          totalData.push(library);
+        }
       }
     });
     saveToDB(totalData);
   }
 });
+
+function printItems(data) {
+  console.log(data);
+}
 
 
 function saveToDB(data) {
