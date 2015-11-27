552eaa25a2bfa6d720b7ab5e2ea9015646c9926c
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2015-11-27 10:15:26 +0200

    Scripts

--- scripts/lib/inputIsil.js
+++ scripts/lib/inputIsil.js
@@ -7,21 +7,16 @@ module.exports = (function() {
   function inputQuery(rl, callback) {
 
     var constructor = require('./libraryConstructor.js');
-    var name = '';
-    var isil = '';
-    var linda = '';
-    var cities = [];
-    var former = '';
     rl.question('Enter the name of the organization: ', function (input) {
-      name = input;
+      var name = input;
       rl.question('Enter the ISIL: ', function (input) {
-        isil = input;
+        var isil = input;
         rl.question('Enter the Linda-code (if any): ', function (input) {
-          linda = input;
+          var linda = input;
           rl.question('Enter the cities (use comma (,) as the separator): ', function (input) {
-            cities = input.split(',');
+            var cities = input.split(',');
             rl.question('If the organization has changed name, enter the old name: ', function (input) {
-              former = input;
+              var former = input;
               var library = constructor.create(name, isil, linda, cities, former);
               callback(library);
             });
