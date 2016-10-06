c5d0f446dc13a9e8a016d14d63dfdadedd151a5f
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2016-10-06 13:22:49 +0300

    Only active codes are shown, hide old library ids

--- src/server/db-query.js
+++ src/server/db-query.js
@@ -2,6 +2,7 @@
 
 'use strict';
 
+const _ = require('underscore');
 const MongoClient = require('mongodb').MongoClient;
 const mongoUrl = 'mongodb://localhost:27017/isil';
 
@@ -28,11 +29,14 @@ function performQuery(req, callback) {
         console.log(logEntry);
         db.collection('data').find(query).toArray( (err, doc) => {
 
+          // Only show entries where the 'active' property is true
           // Parse the cities-array to a string
 
-          doc.forEach(d => {
-            d.cities = d.cities.join(', ');
-          });
+          doc = _.chain(doc)
+                 .filter(entry => { return entry.active === true; })
+                 .map(entry => { entry.cities = entry.cities.join(', '); return entry; })
+                 .value();
+
           db.close();
           callback(doc);
         });
